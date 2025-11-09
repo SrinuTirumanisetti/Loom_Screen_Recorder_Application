9d56ec3b353c3c205837cf6a195980036b40922c
SrinuTirumanisetti <srinutirumanisetti@gmail.com>
2025-11-09 11:08:06 +0530

    designed capture and record buttons

--- index.js
+++ index.js
@@ -7,7 +7,8 @@ let constraints={
 //navigator is global obj this gives information about browser
 //getUserMedia():-With the user's permission through a prompt, turns on a camera and/or a microphone on the system and provides a MediaStream containing a video track and/or an audio track with the input.
 //https://developer.mozilla.org/en-US/docs/Web/API/MediaDevices/getUserMedia
-navigator.mediaDevices.getUserMedia(constraints)
-.then((stream) =>{
-    video.srcObject=stream;
-});
+// navigator.mediaDevices.getUserMedia(constraints)
+// .then((stream) =>{
+//     console.log("streaming");
+//     video.srcObject=stream;
+// });
